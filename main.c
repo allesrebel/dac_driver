e7829aaf75976fe54765df66989d164fdd7aa4bc
Alles Rebel <allesrebel@yahoo.com>
2014-05-01 14:19:22 -0700

    Shortened the time to send a dac command by 1/2 and also added sine stuff in!

--- main.c
+++ main.c
@@ -4,12 +4,30 @@
 void Drive_DAC(unsigned int level);
 void setParameters(int freq, int samples, float duty);
 void initTimer();
+
+//Debugging functions
+void incrementFrequency();
+void incrementState();
+
 /*
- * Pin config - to make it easier to change if needed
+ * Pin configs - to make it easier to change if needed
  */
 #define CS		BIT4
 #define CSPOut	P1OUT
 #define CSPort	P1DIR
+
+//USCI General Port Info
+#define USCIPS1	P1SEL	//USCI Pin mode select
+#define USCIPS2	P1SEL2	//USCI Pin mode select 2
+//UCA0 PORT Defs
+#define UCASOMI	BIT1	//isn't really used to communicate with DAC
+#define UCASIMO	BIT2
+#define UCACLK	BIT4
+
+//UCB0 PORT Defs
+#define UCBSOMI	BIT6	//isn't really used to communicate with DAC
+#define UCBSIMO	BIT7
+#define UCBCLK	BIT5
 
 /*
  * Constants and Variables
@@ -22,10 +40,11 @@ volatile unsigned int current_freq = 0;
 volatile unsigned int sqaure_val = 0;
 volatile unsigned int TempDAC_Value = 0;
 volatile unsigned int sampledCycles[5] = { 1600, 800, 533, 400, 320 };
-volatile unsigned int SIN_Values[50] = { 862, 703, 553, 415, 292, 188, 105, 45,
-		10, 0, 17, 59, 125, 214, 324, 451, 593, 746, 906, 1069, 1230, 1387,
-		1534, 1669, 1787, 1885, 1962, 2015, 2044, 2046, 2022, 1973, 1901, 1805,
-		1691, 1559, 1414, 1259, 1097, 935 }; // Sin values rounded to nearest int.
+volatile unsigned int SIN_Values[50] = { 1024, 1152, 1278, 1400, 1517, 1625,
+		1724, 1813, 1888, 1950, 1997, 2029, 2045, 2045, 2029, 1997, 1950, 1888,
+		1813, 1724, 1625, 1517, 1400, 1278, 1152, 1024, 895, 769, 647, 530, 422,
+		323, 234, 159, 97, 50, 18, 2, 2, 18, 50, 97, 159, 234, 323, 422, 530,
+		647, 769, 895 }; // Sin values rounded to nearest int.
 
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
@@ -49,8 +68,8 @@ int main(void) {
 	//Initialize Ports
 	CSPort |= CS;		//Use bit to activate CE (active low) on the DAC(pin 2)
 
-	P1SEL = BIT7 + BIT5;	// These two lines select UCB0CLK for P1.5
-	P1SEL2 = BIT7 + BIT5;	// and select UCB0SIMO for P1.7
+	USCIPS1 = UCBSIMO + UCBCLK;	// These two lines select UCB0CLK for P1.5
+	USCIPS2 = UCBSIMO + UCBCLK;	// and select UCB0SIMO for P1.7
 
 	UCB0CTL0 |= UCCKPL + UCMSB + UCMST + UCSYNC;
 
@@ -62,33 +81,36 @@ int main(void) {
 	//set up timer with 100Hz initially
 	setParameters(freqs[current_freq], 100, 50);
 
-	_BIS_SR(LPM0_bits + GIE);
+	_enable_interrupts();
 
 	while (1) {
-		setParameters(freqs[current_freq], 100, 50);
+		//setParameters(freqs[current_freq], 100, 50);
 	}
 }
 
+/*
+ * Drive_Dac
+ * Drives the DAC at max possible speed
+ * -note apparently at this speed (16Mhz) the buffer
+ * 	is ready as soon as the command is sent, no waiting really needed
+ * 	just make sure that that enable is held long enough
+ */
 void Drive_DAC(unsigned int level) {
-	unsigned int DAC_Word = 0;
-
-	DAC_Word = (0x1000) | (level & 0x0FFF); // 0x1000 sets DAC for Write
+	//Create the container for the total data that's going to be sent (16 bits total)
+	unsigned int DAC_Word = (0x1000) | (level & 0x0FFF); // 0x1000 sets DAC for Write
 
 	CSPOut &= ~CS; //Select the DAC
+	UCB0TXBUF = (DAC_Word >> 8); // Send in the upper 8 bits
 
-	UCB0TXBUF = (DAC_Word >> 8); // Shift upper byte of DAC_Word
-	// 8-bits to right
-
-	while (!(IFG2 & UCB0TXIFG))
-		; // USCI_A0 TX buffer ready?
+	//would normally wait for buffer to flush, but don't need to at 16Mhz
 
 	UCB0TXBUF = (unsigned char) (DAC_Word & 0x00FF); // Transmit lower byte to DAC
 
-	while (!(IFG2 & UCB0TXIFG))
-		; // USCI_A0 TX buffer ready?
+	//roughly the the time needed to process everything
+	__delay_cycles(10);	//any less and we don't get anything
 
-	CSPOut |= CS; //Unselect the DAC
-	return;
+	//Clock the data in
+	CSPOut |= CS;
 }
 
 /*
@@ -140,31 +162,17 @@ __interrupt void Timer_A(void) {
 		}
 		count++;
 
-		//drive dac somehow
+		//drive dac with sqaure
 		Drive_DAC(sqaure_val);
 		break;
 	case 1:
-		//sine wave
-		SIN_Values[sin_pos];
-		switch (freq) {
-		case 500:
+		if (sin_pos + 1 > 49)
+			sin_pos = 0;
+		else
+			sin_pos++;
 
-			break;
-		case 400:
-			sin_pos++;
-			break;
-		case 300:
-			sin_pos++;
-			break;
-		case 200:
-			sin_pos++;
-			break;
-		case 100:
-			sin_pos++;
-			break;
-		}
-		sin_pos++;
-
+		//drive Dac with sine val
+		Drive_DAC(SIN_Values[sin_pos]);
 		break;
 	case 2:
 		//sawtooth
@@ -173,16 +181,28 @@ __interrupt void Timer_A(void) {
 	}
 }
 
+void incrementFrequency() {
+	if (current_freq + 1 > 4)
+		current_freq = 0;
+	else
+		current_freq++;
+
+	setParameters(freqs[current_freq], 100, 50); //set it
+}
+
+void incrementState() {
+	if (state + 1 > 2)
+		state = 0;
+	else
+		state++;
+}
+
 //Button push interrupt ISR
 #pragma vector=PORT1_VECTOR
 __interrupt void ISR_PORT1(void) {
 	if ((P1IFG & BIT3)== BIT3) {
-		if (current_freq + 1 > 4) {
-			current_freq = -1;
-		}
-		current_freq++;
+		incrementState();
 
-		setParameters(freqs[current_freq], 100, 50); //set it
 		P1IFG &= ~BIT3;	//clear interrupt flag
 	}
 }
